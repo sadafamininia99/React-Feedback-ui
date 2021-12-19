9bd2d6e0f7df021c906176708c03297df3aa8eeb
sadafamininia99 <aminicomputer96@gmail.com>
2021-12-19 22:55:05 +0330

    header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import Header  from "./components/Header"
-function App(){
-     return (
-         <>
-         <Header />
-         <div className="contsiner">
-             <h1>
-                 My app
-             </h1>
-         </div>
-         </>
-     )
- }
- export default App
+import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
+
+
+function App() {
+  return (
+    <>
+      <Header  />
+      <div className="contsiner">
+
+        <FeedbackItem/>
+      </div>
+    </>
+  );
+}
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,28 @@
+import PropTypes from 'prop-types'
 
-function Header({text}){
-    return (
-        <header>
-            <div className="container">
-                <h2>{text}</h2>
-            </div>
-            
-        </header>
-    )
+function Header({ text,bgColor,textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+  return (
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  ); 
 }
 
-Header.defultProps= {
-    text: 'Feedback ui',
+Header.defaultProps = {
+    text: 'Feedback UI',
+    bgColor:'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95',
 }
+Header.propTypes = {
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
+  }
 
-// Header.PropTypes={
-//     text :PropTypes.string,
-// }
-
-export default Header
+export default Header;
