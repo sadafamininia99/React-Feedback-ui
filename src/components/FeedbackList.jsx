ec290bb651aa1098d4c540f14c78c2fc917b1af4
sadafamininia99 <aminicomputer96@gmail.com>
2021-12-20 14:05:31 +0330

    the if statment for check if its true

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,19 @@
+
+import FeedbackItem from "./FeedbackItem";
 function FeedbackList({feedback}) {
-  console.log(feedback)
-  return <div>List </div>;
+  if (!feedback || feedback.lenghth ===0){
+    return <p>no feedback yet</p>
+
+  }
+  return <div className="feedback-list">
+    {feedback.map((item)=>(
+<FeedbackItem key={item.id} item={item} />
+    ))}
+
+
+    
+    
+     </div>;
 }
 
-export default FeedbackList;
+export default FeedbackList
