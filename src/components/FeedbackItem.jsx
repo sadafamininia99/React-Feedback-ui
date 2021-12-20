3046935d52d6bd4c69d59ab7a06e68c5b04090a4
sadafamininia99 <aminicomputer96@gmail.com>
2021-12-20 14:02:24 +0330

    cleaner than before

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
 
-function FeedbackItem() {
-  const [rating, setRating] = useState(8);
-  const [text, setText] = useState("This is an example of a feedback item");
-
-  const handleClick = () => {
-    setRating((prev) => {
-      console.log(prev);
-      return prev + 1;
-    });
-  };
-
+function FeedbackItem({item}) {
+ 
   return (
     <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Click me </button>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
+     
     </div>
   );
 }
