4e6e97693e8fe6722d0a8088424f14464c0b90ab
sadafamininia99 <aminicomputer96@gmail.com>
2021-12-21 15:49:50 +0330

    add button component to (primary)

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Card from "./shared/Card";
+import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
@@ -20,7 +21,9 @@ function FeedbackForm() {
             placeholder="write a review"
             value={text}
           />
-          <button type="submit">Send</button>
+          <Button type="submit" version="primary">
+            Send
+          </Button>
         </div>
       </form>
     </Card>
